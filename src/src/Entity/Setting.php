7579764aba2dadcaf625d2293321b8a066c01520
sfxon <steve@mindfav.com>
2022-11-05 14:23:34 +0100

    Created Setting Entity.

---
+++ src/src/Entity/Setting.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SettingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SettingRepository::class)]
+class Setting
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(length: 64)]
+    private ?string $textId = null;
+
+    #[ORM\Column(length: 65535)]
+    private ?string $value = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getTextId(): ?string
+    {
+        return $this->textId;
+    }
+
+    public function setTextId(string $textId): self
+    {
+        $this->textId = $textId;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}

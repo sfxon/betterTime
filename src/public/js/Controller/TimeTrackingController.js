d4b49f7f55106efd33c97845762fffaabc01e0f9
sfxon <steve@mindfav.com>
2022-11-21 06:37:40 +0100

    Transformed Javascript to Object-oriented format.

--- src/public/js/Controller/TimeTrackingController.js
+++ src/public/js/Controller/TimeTrackingController.js
@@ -1,36 +1,3 @@
-function initActions() {
-    initSearchProjectAction();
-}
-
-initActions();
-
-/* Function Definitions */
-function initSearchProjectAction() {
-    var searchField = document.getElementById('projectSearch');
-
-    if(null == searchField) {
-        return;
-    }
-
-    searchField.addEventListener('paste', function(event) {
-        var searchTerm = event.clipboardData.getData('Text');
-        searchProjects(searchTerm);
-    });
-
-    searchField.addEventListener('keyup', function() {
-        var searchTerm = this.value;
-        searchProjects(searchTerm);
-    });
-
-    searchField.addEventListener('focusout', function() {
-        if(searchProjectTimeout != null) {
-            clearTimeout(searchProjectTimeout);
-            searchProjectTimeout = null;
-        }
-
-        setTimeout(
-            function() { hideElement('#projectSearchResult'); },
-            200
-        );
-    });
-}
+window.addEventListener('DOMContentLoaded', (event) => {
+    dlhProjectComboBox = new DlhComboBoxControl('#projectSearchContainer');
+});
--- src/public/js/Service/ProjectService.js
+++ src/public/js/Service/ProjectService.js
@@ -1,137 +1,217 @@
-var searchProjectTimeout = null;
+class DlhComboBoxControl {
+    constructor(selector) {
+        this.selector = selector;
+        this.containerElem = null;
+        this.searchInputElem = null;
+        this.searchResultElem = null;
+        this.comboboxValueElem = null;
+        this.searchProjectTimeout = null;
 
-function searchProjects(searchTerm)
-{
-    if(searchProjectTimeout !== null) {
-        clearTimeout(searchProjectTimeout);
+        this._onSearchInputPaste = this.onSearchInputPaste.bind(this);
+        this._onSearchInputKeyup = this.onSearchInputKeyup.bind(this);
+        this._onSearchInputFocusout = this.onSearchInputFocusout.bind(this);
+
+        if(this.initSelectors()) {
+            this.initActions();
+        }
     }
 
-    searchProjectTimeout = setTimeout(
-        function() {
-            searchProjectsClearResults();
+    initSelectors() {
+        this.containerElem = document.querySelector(this.selector);
 
-            searchProjectTimeout = null;
-
-            axios.post('/projects/ajaxSearch', {
-                searchTerm: searchTerm
-            })
-            .then(function(response) {
-                try {
-                    var searchResults = response.data.searchResult;
-                    var length = searchResults.length;
-
-                    if(length == 0) {
-                        searchProjectsAddResult("", "Keine Ergebnisse");
-                        makeInputReadonly('#projectSearchResult');
-                    } else {
-                        makeInputWritable('#projectSearchResult');
-
-                        for(var i = 0; i < length; i++) {
-                            var result = searchResults[i];
-
-                            searchProjectsAddResult(result.id, result.name);
-                        }
-
-                        initProjectSearchResultActionHandlers();
-                    }
-
-                    markContainerAsOpened(document.getElementById('projectSearchResult').closest('.dlh-combobox'));
-                    setSearchResultSize(length);
-                    showElement('#projectSearchResult');
-                    
-                } catch(e) {
-                    alert('Fehler...');
-                    console.log(e);
-                }
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
-        },
-        750
-    );
-}
-
-function searchProjectsClearResults() {
-    var results = document.querySelectorAll('#projectSearchResult option');
-
-    if(results.length > 0) {
-        for (var i = (results.length - 1); i >= 0; i--) {
-            results[i].remove();
+        if(this.containerElem !== null) {
+            this.searchInputElem = this.containerElem.querySelector('.dlh-combobox-search');
+            this.searchResultElem = this.containerElem.querySelector('.dlh-combobox-result');
+            this.comboboxValueElem = this.containerElem.querySelector('.dlh-combobox-value');
+            return true;
         }
 
-        hideElement('#projectSearchResult');
+        return false;
     }
-}
 
-function searchProjectsAddResult(id, name) {
-    var searchResultContainer = document.getElementById('projectSearchResult');
+    initActions() {
+        this.initSearchInputPasteEvent();
+        this.initSearchInputKeyupEvent();
+        this.initSearchInputFocusoutEvent();
+    }
 
-    var opt = document.createElement('option');
-    opt.value = id;
-    opt.innerHTML = name;
+    initSearchInputPasteEvent() {
+        if(this.searchInputElem === null) {
+            return;
+        }
 
-    searchResultContainer.appendChild(opt);
-}
+        this.searchInputElem.addEventListener('paste', this._onSearchInputPaste);
+    }
 
-function showElement(selector) {
-    document.querySelector(selector).style.display = 'block';
-}
+    onSearchInputPaste(event) {
+        var searchTerm = event.clipboardData.getData('Text');
+        this.searchProjects(searchTerm);
+    }
 
-function hideElement(selector) {
-    document.querySelector(selector).style.display = 'none';
-}
+    initSearchInputKeyupEvent() {
+        if(this.searchInputElem === null) {
+            return;
+        }
 
-function initProjectSearchResultActionHandlers() {
-    var results = document.querySelectorAll('#projectSearchResult option');
+        this.searchInputElem.addEventListener('keyup', this._onSearchInputKeyup);
+    }
 
-    for (var i = 0; i < results.length; i++) {
-        let elem = results[i];
+    onSearchInputKeyup(event) {
+        var searchTerm = event.target.value;
+        this.searchProjects(searchTerm);
+    }
 
-        elem.addEventListener('click', function() {
-            document.getElementById('projectSearch').value = elem.text;
-            document.getElementById('projectId').value = elem.value;
-            hideElement('#projectSearchResult');
-            markContainerAsClosed(document.getElementById('projectSearchResult').closest('.dlh-combobox'))
+    initSearchInputFocusoutEvent() {
+        if(this.searchInputElem === null) {
+            return;
+        }
+
+        this.searchInputElem.addEventListener('focusout', this._onSearchInputFocusout);
+    }
+
+    onSearchInputFocusout() {
+        if(this.searchProjectTimeout != null) {
+            clearTimeout(this.searchProjectTimeout);
+            this.searchProjectTimeout = null;
+        }
+
+        setTimeout(
+            () => { this.hideElement(this.searchResultElem); },
+            200
+        );
+    }
+
+    resetTimeout() {
+        if(this.searchProjectTimeout !== null) {
+            clearTimeout(this.searchProjectTimeout);
+            this.searchProjectTimeout = null;
+        }
+    }
+
+    searchProjects(searchTerm) {
+        this.resetTimeout();
+
+        this.searchProjectTimeout = setTimeout(
+            () => {
+                this.searchProjectsAjax(searchTerm);
+            },
+            750
+        );
+    }
+
+    searchProjectsAjax(searchTerm) {
+        this.searchProjectsClearResults();
+        this.searchProjectTimeout = null;
+
+        axios.post('/projects/ajaxSearch', {
+            searchTerm: searchTerm
+        })
+        .then(function(response) {
+            try {
+                var searchResults = response.data.searchResult;
+                var length = searchResults.length;
+
+                if(length == 0) {
+                    this.searchProjectsAddResult("", "Keine Ergebnisse");
+                    this.makeInputReadonly(this.searchResultElem);
+                } else {
+                    this.makeInputWritable(this.searchResultElem);
+
+                    for(var i = 0; i < length; i++) {
+                        let result = searchResults[i];
+                        this.searchProjectsAddResult(result.id, result.name);
+                    }
+
+                    this.initProjectSearchResultActionHandlers();
+                }
+
+                this.markContainerAsOpened(this.containerElem);
+                this.setSearchResultSize(length);
+                this.showElement(this.searchResultElem);
+            } catch(e) {
+                alert('An error occured, please try again. Check console for error details.');
+                console.log(e);
+            }
+        }.bind(this))
+        .catch(function(error) {
+            alert('(2)An error occured, please try again. Check console for error details.');
+            console.log(error);
         });
     }
-}
 
-function markContainerAsOpened(element) {
-    element.classList.add('opened');
-}
-
-function markContainerAsClosed(element) {
-    element.classList.remove('opened');
-}
-
-function setSearchResultSize(size) {
-    if(!Number.isInteger(size)) {
-        size = 2;
+    searchProjectsClearResults() {
+        var results = this.searchResultElem.querySelectorAll('option');
+    
+        if(results.length > 0) {
+            for (var i = (results.length - 1); i >= 0; i--) {
+                results[i].remove();
+            }
+    
+            this.hideElement(this.searchResultElem);
+        }
     }
     
-    // Height must be 2 at least, to show an opened dropdown.
-    if(size < 2) {
-        size = 2;
-    } else if(size > 10) {
-        size = 10;
+    searchProjectsAddResult(id, title) {
+        var opt = document.createElement('option');
+        opt.value = id;
+        opt.innerHTML = title;
+        this.searchResultElem.appendChild(opt);
+    }
+    
+    showElement(elem) {
+        elem.style.display = 'block';
+    }
+    
+    hideElement(elem) {
+        elem.style.display = 'none';
+    }
+    
+    initProjectSearchResultActionHandlers() {
+        var results = this.searchResultElem.querySelectorAll('option');
+    
+        for (var i = 0, j = results.length; i < j; i++) {
+            let elem = results[i];
+
+            this._onSearchResultOptionClick = this.onSearchResultOptionClick.bind(this);
+            elem.addEventListener('click', this._onSearchResultOptionClick);
+        }
     }
 
-    document.getElementById('projectSearchResult').size = size;
-}
-
-function makeInputReadonly(selector) {
-    var elements = document.querySelectorAll(selector);
-
-    for(var i = 0; i < elements.length; i++) {
-        elements[i].setAttribute("disabled", "disabled");
+    onSearchResultOptionClick(event) {
+        this.searchInputElem.value = event.target.text;
+        this.comboboxValueElem.value = event.target.value;
+        this.hideElement(this.searchResultElem);
+        this.markContainerAsClosed(this.containerElem);
     }
-}
-
-function makeInputWritable(selector) {
-    var elements = document.querySelectorAll(selector);
-
-    for(var i = 0; i < elements.length; i++) {
-        elements[i].removeAttribute("disabled");
+    
+    markContainerAsOpened(elem) {
+        elem.classList.add('opened');
+    }
+    
+    markContainerAsClosed(elem) {
+        elem.classList.remove('opened');
+    }
+    
+    setSearchResultSize(size) {
+        if(!Number.isInteger(size)) {
+            size = 2;
+        }
+        
+        // Height must be 2 at least, to show an opened dropdown.
+        if(size < 2) {
+            size = 2;
+        } else if(size > 10) {
+            size = 10;
+        }
+    
+        this.searchResultElem.size = size;
+    }
+    
+    makeInputReadonly(elem) {
+        elem.setAttribute("disabled", "disabled");
+    }
+    
+    makeInputWritable(elem) {
+        elem.removeAttribute("disabled");
     }
 }

4736390b1d423d05f70b04b63865d0b717fa19d3
sfxon <steve@mindfav.com>
2022-11-24 12:50:00 +0100

    Aded datePicker to the start- and end- DateTime Controls in the endProjectTime Dialog.

--- src/public/js/Controller/TimeTrackingController.js
+++ src/public/js/Controller/TimeTrackingController.js
@@ -1,3 +1,41 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    dlhProjectComboBox = new DlhComboBoxControl('#projectSearchContainer');
+    const dlhProjectComboBox = new DlhComboBoxControl('#projectSearchContainer');
+
+    const dateTimeStartInput = document.getElementById('starttime');
+
+    const dateTimeStart = new tempusDominus.TempusDominus(
+        dateTimeStartInput,
+        {
+            display: {
+                sideBySide: true
+            },
+        }
+    );
+
+    const dateTimeEnd = new tempusDominus.TempusDominus(
+        document.getElementById('endtime'),
+        {
+            display: {
+                sideBySide: true
+            },
+
+        }
+    );
+
+    dateTimeStartInput.addEventListener('change', (e) => {
+        dateTimeEnd.updateOptions({
+            restrictions: {
+                minDate: e.detail.date,
+            },
+        });
+    });
+      
+      //using subscribe method
+    const subscription = dateTimeEnd.subscribe('change', (e) => {
+        dateTimeStart.updateOptions({
+            restrictions: {
+                maxDate: e.date,
+            },
+        });
+    });
 });

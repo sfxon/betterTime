c2f8b8de0cc72c599ca90e0a89bd13afad052a3a
sfxon <steve@mindfav.com>
2022-12-08 06:33:26 +0100

    Fixed redirect after login. Will now always redirect to the user-dashboard.

--- src/src/Security/CustomAuthenticator.php
+++ src/src/Security/CustomAuthenticator.php
@@ -42,10 +42,6 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
-
         return new RedirectResponse($this->urlGenerator->generate('app_dashboard'));
     }
 

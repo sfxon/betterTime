75d6bf3aa70c11626d11b5756bba33948fe2804d
sfxon <steve@mindfav.com>
2023-01-04 07:51:39 +0100

    Refactored Service\ConfigService.   * Method loadConfig has been splitted into several smaller methods, to be easier to understand and test.   * Checked comments and variable-type declarations and updated them, where needed.   * Updated scope of variables and constants.   * Ordered methods by visibility and method name.   * Removed unused variables.

--- src/src/Service/ConfigService.php
+++ src/src/Service/ConfigService.php
@@ -11,14 +11,17 @@ use App\Repository\ConfigValueRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Uid\Uuid;
 
+/**
+ * Load configuration values from the database
+ * by priority and filters.
+ */
 class ConfigService
 {
     private ManagerRegistry $doctrine;
     private ConfigDefinitionRepository $configDefinitionRepository;
     private ConfigValueRepository $configValueRepository;
-    private User $user;
-
-    private $levelPriority = [
+    
+    const levelPriority = [
         'initial' => 10,
         'system' => 20,
         'backend' => 30,
@@ -38,69 +41,18 @@ class ConfigService
         $this->configDefinitionRepository = $this->doctrine->getRepository(ConfigDefinition::class);
         $this->configValueRepository = $this->doctrine->getRepository(ConfigValue::class);
     }
-        
+    
     /**
-     * Load configuration value.
-     * The function tries to load a given configuration
-     * over certain config-levels. It begins with the highest
-     * and goes one step down everytime it doesn't find
-     * a configuration for a certain level.
-     *
-     * @param  string $technicalName
-     * @param  array $availableLevels
-     * @return mixed|null
+     * Filters an array of ConfigValue entries.
+     * 
+     * @param array $priorityArray      Must be an indexed array of `ConfigValue`; The index is expected to be an existing value of the self::PriorityLevel array.
+     * @param ?array $availableLevels   If this is null, only empty `ConfigValue` entries are filtered out. If it is not null, only those entries of $priorityArray are returned, that have a key, that is set in $availableLevels.
+     * @return array
      */
-    public function loadConfig(
-        string $technicalName,
-        array $availableLevels = null,
-        User $user = null
-    ): mixed {
-        $configDefinition = $this->loadConfigDefinition($technicalName);
-
-        // Load user configuration if wanted and available.
-        if (
-            null !== $availableLevels &&
-            in_array('user', $availableLevels) &&
-            $user !== null
-        ) {
-            if(null === $user) {
-                throw new \RuntimeException('User not found');
-            }
-
-            $configValue = $this->loadUserConfigValue($technicalName, $user->getId());
-
-            if($configValue !== null) {
-                return $configValue->getValue();
-            }
-        }
-
-        // TODO: Implement same check for teams as for user level, as soon as teams are implemented. (There is no implementation yet).
-
-        // Load config values for config definition.
-        $configValues = $this->configValueRepository->findBy([ 
-            'configDefinitionId' => $configDefinition->getId(),
-            'foreignId' => null
-        ]);
-
-        if($configValues === null) {
-            return null;
-        }
-
-        // Sort keys by priority
-        $priorityArray = $this->getPriorityArray();
-
-        foreach($configValues as $configValue) {
-            $priority = $configValue->getLevel();
-
-            if(!array_key_exists($priority, $priorityArray)) {
-                throw new \RuntimeException('An undefined level has been loaded from the configuration database');
-            }
-
-            $priorityArray[$priority] = $configValue;
-        }
-
-        // Sort out unwanted keys and entries, where no data has been loaded,
-        // but only, if filtering is wanted.
+    public function filterConfigValueArray(
+        array $priorityArray, 
+        ?array $availableLevels): array
+    {
         $finalValues = [];
 
         if (null === $availableLevels) {
@@ -122,6 +74,71 @@ class ConfigService
             }
         }
 
+        return $finalValues;
+    }
+
+    /**
+     * Prepares a initial priority array, based on the clas constant levelPriority.
+     * The values are all null, so other methods can use this to check, if values have been loaded or not.
+     * (If they are null, they probably have not been loaded.)
+     *
+     * @return array
+     */
+    public static function getPriorityArray(): array
+    {
+        $retval = [];
+
+        foreach(self::levelPriority as $key => $priority) {
+            $retval[$key] = null;
+        }
+
+        return $retval;
+    }
+        
+    /**
+     * Load configuration value.
+     * The function tries to load a given configuration
+     * over certain config-levels. It begins with the highest
+     * and goes one step down everytime it doesn't find
+     * a configuration for a certain level.
+     *
+     * @param  string $technicalName
+     * @param  array $availableLevels
+     * @return mixed|null
+     */
+    public function loadConfig(
+        string $technicalName,
+        array $availableLevels = null,
+        User $user = null
+    ): mixed {
+        $configDefinition = $this->loadConfigDefinition($technicalName);
+
+        // Load user configuration if wanted and available.
+        $configValue = $this->loadUserConfig($technicalName, $availableLevels, $user);
+
+        if($configValue !== null) {
+            return $configValue;
+        }
+
+        // TODO: Implement same check for teams as for user level, as soon as teams are implemented. (There is no implementation yet).
+
+        // Load config values for config definition.
+        $configValues = $this->configValueRepository->findBy([ 
+            'configDefinitionId' => $configDefinition->getId(),
+            'foreignId' => null
+        ]);
+
+        if($configValues === null) {
+            return null;
+        }
+
+        // Sort keys by priority
+        $priorityArray = $this->sortConfigValueArray($configValues);
+
+        // Filter out unwanted keys and entries, where no data has been loaded,
+        // but only, if filtering is wanted.
+        $finalValues = $this->filterConfigValueArray($priorityArray, $availableLevels);
+
         if (count($finalValues) == 0) {
             return null;
         }
@@ -130,6 +147,25 @@ class ConfigService
         $configValue = array_pop($finalValues);
 
         return $configValue->getValue();
+    }
+
+    /**
+     * Load ConfigDefinition from database.
+     *
+     * @param  string $technicalName
+     * @return ConfigDefinition
+     */
+    public function loadConfigDefinition(string $technicalName): ConfigDefinition
+    {
+        $configDefinition = $this->configDefinitionRepository->findOneBy([ 
+            'technicalName' => $technicalName
+        ]);
+
+        if(null === $configDefinition) {
+            throw new ConfigDefinitionNotFoundException('No ConfigDefinition with technical name "' . $technicalName . '" found.');
+        }
+
+        return $configDefinition;
     }
     
     /**
@@ -162,37 +198,57 @@ class ConfigService
     }
     
     /**
-     * getPriorityArray
+     * Sorts an array of configValues by there priority.
+     * Returns a sorted associative array, with the priority as key.
      *
+     * @param  array $configValues
      * @return array
      */
-    private function getPriorityArray(): array
+    public function sortConfigValueArray(array $configValues): ?array
     {
-        $retval = [];
+        $priorityArray = self::getPriorityArray();
 
-        foreach($this->levelPriority as $key => $priority) {
-            $retval[$key] = null;
+        foreach ($configValues as $configValue) {
+            $priority = $configValue->getLevel();
+
+            if (!array_key_exists($priority, $priorityArray)) {
+                throw new \RuntimeException('An undefined level has been provided as key.');
+            }
+
+            $priorityArray[$priority] = $configValue;
         }
 
-        return $retval;
+        return $priorityArray;
     }
     
     /**
-     * Load ConfigDefinition from database.
+     * Load user configuration.
      *
-     * @param  string $technicalName
-     * @return ConfigDefinition
+     * @param string $technicalName
+     * @param ?array $availableLevels
+     * @return ?string
      */
-    private function loadConfigDefinition(string $technicalName): ConfigDefinition
+    private function loadUserConfig(
+        string $technicalName,
+        ?array $availableLevels,
+        ?User $user): ?string
     {
-        $configDefinition = $this->configDefinitionRepository->findOneBy([ 
-            'technicalName' => $technicalName
-        ]);
+        if (
+            null !== $availableLevels &&
+            in_array('user', $availableLevels) &&
+            $user !== null
+        ) {
+            if(null === $user) {
+                throw new \RuntimeException('User not found');
+            }
 
-        if(null === $configDefinition) {
-            throw new ConfigDefinitionNotFoundException('No ConfigDefinition with technical name "' . $technicalName . '" found.');
+            $configValue = $this->loadUserConfigValue($technicalName, $user->getId());
+
+            if($configValue !== null) {
+                return $configValue->getValue();
+            }
         }
 
-        return $configDefinition;
+        return null;
     }
 }
